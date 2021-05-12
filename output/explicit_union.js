629a3c85d124a74b0d0f098b7c69426a7ea150bd
mkstamin <mkstamincse@gmail.com>
2021-05-12 11:35:19 +0600

    added .gitignor worked in explicit and union

---
+++ output/explicit_union.js
@@ -0,0 +1,29 @@
+"use strict";
+var a;
+var b;
+a = "Mks";
+b = 39;
+/*
+ ************* array ************
+ */
+// let arr: Array<string> = [];
+var arr = [];
+arr.push(40);
+arr.push("Any");
+/*
+ ************* object ************
+ */
+var obj;
+obj = {
+    name: "Mks",
+    age: 25,
+    arr: [2, 3, 4, 5],
+};
+// obj = [2, 4, 5, 6];
+var obj1;
+obj1 = {
+    name: "Mks",
+    age: 25,
+    isStudent: true,
+    //   arr: [2, 3, 4, 5],
+};
